19e91ba0d0eea337febb97fb02b093441dc1e5cc
AhmedOg <ahmedogbinto@gmail.com>
2022-04-18 17:29:15 -0400

    Arguments et valeur de retour

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
-public class Main
-{
+public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Voiture voitureDeAhmed = new Voiture();
-        voitureDeAhmed.nbPortes=3;
-        voitureDeAhmed.automatique=true;
-        voitureDeAhmed.couleur="Vert";
+        voitureDeAhmed.nbPortes = 3;
+        voitureDeAhmed.automatique = true;
+        voitureDeAhmed.couleur = "Vert";
 
-        System.out.println("la voiture de Ahmed est "+voitureDeAhmed.couleur+" et elle a "+voitureDeAhmed.nbPortes+" portes");
+        System.out.println("la voiture de Ahmed est " + voitureDeAhmed.couleur + " et elle a " + voitureDeAhmed.nbPortes + " portes");
+
         voitureDeAhmed.klaxonner();
+
+        voitureDeAhmed.acceler();
+        System.out.println(voitureDeAhmed.acceler()+" à l'heure");
+
+        int nouvelleVitesse = voitureDeAhmed.acceler();
+        System.out.println("Nouvelle vitesse d'accélération: "+nouvelleVitesse+ " km/h");
+
+        int nouvelleVitesseCourante = voitureDeAhmed.passerVitesse(true);
+
+
     }
 }
--- src/Voiture.java
+++ src/Voiture.java
@@ -3,9 +3,27 @@ public class Voiture
     int nbPortes=5;
     boolean automatique;
     String couleur;
+    int vitesseCourante;
 
     void klaxonner()
     {
         System.out.println("tutu!!!!!!");
+    }
+    int acceler()
+    {
+        System.out.println("J'accélère");
+        return 100;
+    }
+    int passerVitesse(boolean enVitesse)
+    {
+        if (enVitesse)
+        {
+            vitesseCourante++;
+        }
+        else
+        {
+           vitesseCourante-- ;
+        }
+        return vitesseCourante;
     }
 }
