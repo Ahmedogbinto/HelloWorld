08029d9de3cabf07dbf4c226f2a8fd592d5d282e
AhmedOg <ahmedogbinto@gmail.com>
2022-04-20 19:08:11 -0400

    Constructeurs sans parametres, constructeurs par defaut et ajout dautres constructeurs.

--- src/Moteur.java
+++ src/Moteur.java
@@ -2,4 +2,5 @@ public class Moteur
 {
     String carburation;
     int cylindre;
+    int nbCylindre;
 }
--- src/Voiture.java
+++ src/Voiture.java
@@ -1,13 +1,48 @@
 public class Voiture
 {
+    static int nbroue=4;
     int nbPortes=5;
     boolean automatique;
-    String couleur;
     int vitesseCourante;
     int vitesse;
+    String couleur;
     Moteur  moteur;
-    static int nbroue=4;
+    Voiture()
+    {
+        System.out.println("Une voiture est en construction sans parametre");
+    }
+    Voiture(String couleur)
+    {
+        this.couleur=couleur;
+        System.out.println("Une voiture est en construction avec couleur");
+    }
 
+    Voiture(Moteur moteur)
+    {
+        this.moteur=moteur;
+        System.out.println("Une voiture est en construction avec le moteur");
+    }
+    Voiture(String carburation, int nbCylindre)
+    {
+        Moteur moteur = new Moteur();
+        moteur.carburation = carburation;
+        moteur.nbCylindre = nbCylindre;
+        this.moteur = moteur;
+    }
+
+    static void tourner(boolean droite, int angle)
+    {
+        String droiteOuGauche;
+        if (droite)
+        {
+            droiteOuGauche = "droite";
+        }
+        else
+        {
+            droiteOuGauche = "gauche";
+        }
+        System.out.println("Les "+Voiture.nbroue+"roues  tournent a "+droiteOuGauche+" à un angle de "+angle);
+    }
     static void klaxonner()
     {
         System.out.println("tutu!!!!!!");
@@ -36,21 +71,6 @@ public class Voiture
         }
         return vitesseCourante;
     }
-
-    static void tourner(boolean droite, int angle)
-    {
-        String droiteOuGauche;
-        if (droite)
-        {
-            droiteOuGauche = "droite";
-        }
-        else
-        {
-            droiteOuGauche = "gauche";
-        }
-        System.out.println("Les "+Voiture.nbroue+"roues  tournent a "+droiteOuGauche+" à un angle de "+angle);
-    }
-
     Ville transporter(Passager passager, Ville villeDeDepart)
     {
         System.out.println("Je transporte un passager qui S'appelle "+passager.prenom+" " +passager.nom);
