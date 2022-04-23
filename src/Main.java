e46fba1fa7a2066f66df758689d86011b8323efa
AhmedOg <ahmedogbinto@gmail.com>
2022-04-23 18:43:15 -0400

    Notion de tableau multi dimentionnel

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Voiture peugeot206 = new Voiture();
+        int[][] tableauEntier = new int[3][5];
+        tableauEntier [0][1]=12;
+        for (int col=0; col<tableauEntier.length;col++){
+            for(int ligne=0;ligne<tableauEntier[col].length;ligne++)
+                System.out.println("Lelement contenu a la position "+col+" "+ligne+ " est "+tableauEntier[col][ligne]);
+        }
 
 
-        Passager passager = new Passager();
-        passager .nom ="Dupond";
-        passager.prenom="Michel";
-
-        Ville depart = new Ville();
-        depart.nom="Aukland";
-
-        Ville etape1 = new Ville();
-        etape1.nom = "Taupo";
-
-        Ville etape2 = new Ville();
-        etape2.nom = "wellington";
-
-        Ville etape3 = new Ville();
-        etape3.nom = "hallmington";
-
-        peugeot206.transporter(passager, depart, etape1, etape2, etape3);
 
 
 
